ac70608990ff0e12b2ebb0166c790f0626e578eb
77oaking <978aho@gmail.com>
2025-03-20 19:43:56 +0600

    Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,6 +33,46 @@ resumePortfolioTabsBtns.forEach((resumePortfolioTabsBtn, resumeTabClick) => {
 
 
 });
+
+
+
+
+const serviceCardWithModals= document.querySelectorAll(".service-container .card-with-modal");
+
+serviceCardWithModals.forEach((serviceCardWithModal) => {
+  const serviceCard= serviceCardWithModal.querySelector(".service-card");
+  const serviceBackDrop= serviceCardWithModal.querySelector(".service-modal-backdrop");
+  const modalCloseBtn= serviceCardWithModal.querySelector(".modal-close-btn");
+  const serviceModal= serviceCardWithModal.querySelector(".service-modal");
+
+  serviceCard.addEventListener('click', () => {
+   serviceBackDrop.style.display= "flex";
+   setTimeout(() => {
+      serviceBackDrop.classList.add("active");
+   }, 100);
+   setTimeout(() => {
+      serviceModal.classList.add("active");
+   }, 1000);
+   
+   
+  });
+  modalCloseBtn.addEventListener('click', () => {
+   setTimeout(() => {
+      serviceBackDrop.style.display= "none";
+   }, 500);
+   setTimeout(() => {
+      
+      serviceBackDrop.classList.remove("active");
+      serviceModal.classList.remove("active");
+   }, 100);
+
+
+
+   
+  });
+});
+
+
 /* =====================================================
    Service modal open/close function
 ===================================================== */
